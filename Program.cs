3cb212bd9934ef80b235aeae8bf61c37802cd31b
DrTau <drttau@gmail.com>
2021-10-29 15:35:41 +0300

    Writes all dirs and files in inputed directory

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.IO;
 
 namespace laba1_miniTerminal
 {
     class Program
     {
+        static DirectoryInfo currentDirectory = new DirectoryInfo(path: Directory.GetCurrentDirectory());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputDir = Console.ReadLine();
+            currentDirectory = new DirectoryInfo(path: inputDir);
+
+            foreach (var dirInfo in currentDirectory.GetDirectories())
+            {
+                Console.WriteLine(dirInfo.Name);
+            }
+
+            foreach (var fileInfo in currentDirectory.GetFiles())
+            {
+                Console.WriteLine(fileInfo.Name);
+            }
         }
     }
 }
