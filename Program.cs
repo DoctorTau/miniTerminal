cba4b68126761336bdc77cfb96db2c6782d1df91
DrTau <drttau@gmail.com>
2021-10-29 17:25:22 +0300

    depth input

--- Program.cs
+++ Program.cs
@@ -8,17 +8,23 @@ namespace laba1_miniTerminal
         static DirectoryInfo currentDirectory = new DirectoryInfo(path: Directory.GetCurrentDirectory());
         static void Main(string[] args)
         {
+            Console.WriteLine("Input directiry: ");
             string inputDir = Console.ReadLine();
+            Console.WriteLine("Input depth: ");
+            int depth = Convert.ToInt32(Console.ReadLine());
+
             currentDirectory = new DirectoryInfo(path: inputDir);
+
+            Console.WriteLine(currentDirectory.Name);
 
             foreach (var dirInfo in currentDirectory.GetDirectories())
             {
-                Console.WriteLine(dirInfo.Name);
+                Console.WriteLine("[" + dirInfo.FullName + "]");
             }
 
             foreach (var fileInfo in currentDirectory.GetFiles())
             {
-                Console.WriteLine(fileInfo.Name);
+                Console.WriteLine(fileInfo.Name + " (" + (fileInfo.Length / 8) + " B)");
             }
         }
     }
